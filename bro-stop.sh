a92a8b0db0869ee966e6868f6bef79abb3e43642
Brendan Sturges <BrendanSturges@users.noreply.github.com>
2021-05-25 05:32:44 +0000

    its better if we group them together, that's not a better night tbh

--- bro-stop.sh
+++ bro-stop.sh
@@ -1,19 +1,16 @@
 # add this to cron, run it every 3 minutes - pass it your OAUTH token
-# */3 * * * * ./bro-stop.sh TOKEN TOKEN
+# */3 * * * * ./bro-stop.sh TOKEN
+
+dudeyoucansee40fromhere = "Papa Roach", "Korn", "Linkin Park", "Adema"
 
 recent = curl -X "GET" "https://api.spotify.com/v1/me/player/recently-played?limit=10" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer $1" 
 
-if recent.items.track[0].album.artists.name = "Papa Roach"
-    if recent.items.track[1].album.artists.name = "Papa Roach"
+if [[ " ${recent[0]} " =~ " ${dudeyoucansee40fromhere} " ]]; then
+    if [[ " ${recent[1]} " =~ " ${dudeyoucansee40fromhere} " ]]; then
         shutdown -k "bro. you gotta stop."
-        if recent.items.track[2].album.artists.name = "Papa Roach"
-            # turn off everything
-            shutdown -t 60 "you cant be trusted. drink some water"
-
-if recent.items.track[0].album.artists.name = "Korn"
-    hutdown -k "bro. you gotta stop."
-    if recent.items.track[0].album.artists.name = "Korn"
-        # turn off everything
+     if [[ " ${recent[3]} " =~ " ${dudeyoucansee40fromhere} " ]]; then 
         shutdown -t 60 "you cant be trusted. drink some water"
+# whatever you want to do when array contains value
+fi
 
 # this is super bad and not optimized but I'm drunk on a monday night so uh. deal with it.
